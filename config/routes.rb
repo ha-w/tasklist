09beda679466598dc88a1e58c71c94d35b376f67
h.w <ijokij@gmail.com>
2017-11-26 08:22:47 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
 
-root to: 'tasklists#index'
-resources :tasklists
+  get 'users/index'
 
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  root to: 'toppages#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
